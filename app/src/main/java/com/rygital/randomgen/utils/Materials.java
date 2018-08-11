7bbd1d38ba588e0047cfd61dabbdce2625a7bec1
Bendezu Vladislav <bendezuvlad@gmail.com>
2018-08-11 18:31:56 +0300

    Update colors

--- app/src/main/java/com/rygital/randomgen/utils/Materials.java
+++ app/src/main/java/com/rygital/randomgen/utils/Materials.java
@@ -5,14 +5,14 @@ import android.graphics.Color;
 public class Materials {
 
     public static int[][] COLORS = new int[][]{
-             { 0, 0, 0 },
-    /*WATER*/{ Color.rgb(33, 150, 243), Color.rgb(66, 165, 245), Color.rgb(100, 181, 246)},
-    /*LAVA*/ { Color.rgb(244, 67, 54), Color.rgb(255, 111, 0), Color.rgb(244, 81, 30)},
-    /*STONE*/{ Color.rgb(96, 125, 139), Color.rgb(94, 123, 137), Color.rgb(98, 127, 141)},
-    /*GRASS*/{ Color.rgb(124, 179, 66), Color.rgb(139, 195, 74), Color.rgb(156, 204, 101)},
-    /*SAND*/ { Color.rgb(255, 213, 79), Color.rgb(255, 214, 69), Color.rgb(255, 225, 59)},
-    /*MUD*/  { Color.rgb(121, 85, 72), Color.rgb(110, 75, 62), Color.rgb(100, 65, 52)},
-    /*OBSIDIAN*/{Color.rgb(103, 58, 183), Color.rgb(104, 59, 184), Color.rgb(105, 60, 185)}
+                { 0, 0, 0 },
+    /*WATER*/   { Color.rgb(33, 150, 243), Color.rgb(66, 165, 245), Color.rgb(100, 181, 246)},
+    /*LAVA*/    { Color.rgb(244, 67, 54), Color.rgb(255, 111, 0), Color.rgb(244, 81, 30)},
+    /*STONE*/   { Color.rgb(96, 125, 139), Color.rgb(94, 123, 137), Color.rgb(98, 127, 141)},
+    /*GRASS*/   { Color.rgb(124, 179, 66), Color.rgb(139, 195, 74), Color.rgb(156, 204, 101)},
+    /*SAND*/    { Color.rgb(255, 213, 79), Color.rgb(255, 214, 69), Color.rgb(255, 225, 59)},
+    /*MUD*/     { Color.rgb(121, 85, 72), Color.rgb(110, 75, 62), Color.rgb(100, 65, 52)},
+    /*OBSIDIAN*/{Color.rgb(69, 40, 77), Color.rgb(67, 38, 75), Color.rgb(64, 36, 73)}
     };
 
     public static int NOTHING = 0;
