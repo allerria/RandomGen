ac751a48a2e53c84fd820a75cfe2a65788c84c08
azinakov <ab.zinakov@yandex.ru>
2018-08-11 11:59:15 +0300

    last update

--- app/src/main/java/com/rygital/randomgen/MainActivity.java
+++ app/src/main/java/com/rygital/randomgen/MainActivity.java
@@ -1,13 +1,20 @@
 package com.rygital.randomgen;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new DrawView(this));
     }
+
+
 }
