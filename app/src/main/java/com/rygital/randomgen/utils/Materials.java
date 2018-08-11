8737e775e6953f2b43291814fc11f90ed071e860
Bendezu Vladislav <bendezuvlad@gmail.com>
2018-08-11 18:16:30 +0300

    Update colors

--- app/src/main/java/com/rygital/randomgen/utils/Materials.java
+++ app/src/main/java/com/rygital/randomgen/utils/Materials.java
@@ -8,10 +8,10 @@ public class Materials {
              { 0, 0, 0 },
     /*WATER*/{ Color.rgb(33, 150, 243), Color.rgb(66, 165, 245), Color.rgb(100, 181, 246)},
     /*LAVA*/ { Color.rgb(244, 67, 54), Color.rgb(255, 111, 0), Color.rgb(244, 81, 30)},
-    /*STONE*/{ Color.rgb(120, 144, 156), Color.rgb(120, 144, 156), Color.rgb(120, 144, 156)},
-    /*GRASS*/{ Color.rgb(124, 179, 66), Color.rgb(76, 175, 80), Color.rgb(174, 234, 0)},
-    /*SAND*/ { Color.rgb(255, 213, 79), Color.rgb(255, 214, 0), Color.rgb(255, 235, 59)},
-    /*MUD*/  { Color.rgb(121, 85, 72), Color.rgb(93, 64, 55), Color.rgb(62, 39, 35)}
+    /*STONE*/{ Color.rgb(120, 144, 156), Color.rgb(110, 134, 146), Color.rgb(100, 124, 136)},
+    /*GRASS*/{ Color.rgb(124, 179, 66), Color.rgb(114, 175, 76), Color.rgb(174, 189, 56)},
+    /*SAND*/ { Color.rgb(255, 213, 79), Color.rgb(255, 214, 69), Color.rgb(255, 225, 59)},
+    /*MUD*/  { Color.rgb(121, 85, 72), Color.rgb(110, 75, 62), Color.rgb(100, 65, 52)}
     };
 
     public static int NOTHING = 0;
